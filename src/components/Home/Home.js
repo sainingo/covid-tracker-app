b2ed4ebc9a714b3b89b54597d4f17a6a6871450c
sainingo <nickmentor421@gmail.com>
2022-05-23 17:33:40 +0300

    Style the Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './Home.css';
 
 const Home = () => (
-  <div className="continent-row">
+  <div className="continent-row grid">
+    <div className="row-data shadow p-3 mb-5 rounded">
+      <h2>Africa</h2>
+      <p>
+        Total Deaths:
+        {' '}
+        <span> 50000</span>
+      </p>
+      <i role="button" aria-label="Mute volume" className="bi bi-arrow-right-circle" />
+    </div>
+    <div className="row-data shadow p-3 mb-5 rounded">
+      <h2>Africa</h2>
+      <p>Total Deaths: 50000</p>
+    </div>
+    <div className="row-data shadow p-3 mb-5 rounded">
+      <h2>Africa</h2>
+      <p>Total Deaths: 50000</p>
+    </div>
     <div className="row-data shadow p-3 mb-5 rounded">
       <h2>Africa</h2>
       <p>Total Deaths: 50000</p>
