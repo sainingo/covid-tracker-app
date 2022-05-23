66b90c1a62420f70aae92bf6e78195b4ef07007b
sainingo <nickmentor421@gmail.com>
2022-05-23 13:10:33 +0300

    Initialize the app & install the required tools

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl text-center font-bold underline">
+        Covid tracker App
+      </h1>
+    </div>
+  );
+}
+
+export default App;
