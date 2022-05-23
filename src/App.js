8a2bcc1e4cbd46b3311b01f85d88ce839a01f0ac
sainingo <nickmentor421@gmail.com>
2022-05-23 18:43:21 +0300

    Design the details component & add styling to it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Details from './components/Details/Details';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 
@@ -6,7 +8,10 @@ function App() {
   return (
     <div>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = () => (
   <div>
     <nav className="navbar bg-light">
       <div className="container-fluid">
-        <a href className="navbar-brand">Covid Tracker</a>
+        <a href="/" className="navbar-brand">Covid Tracker</a>
         <form className="d-flex" role="search">
           <input className="form-control me-4" type="search" placeholder="Search your country" aria-label="Search" />
           <button className="btn btn-outline-success" type="submit">Search</button>
